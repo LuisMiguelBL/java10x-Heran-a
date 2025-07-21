d9ef466cc6a9d31f6bff6b8f289d65d0ba57726b
Luís Miguel Belfort <vilanovamiguel1@gmail.com>
2025-07-21 16:22:06 -0300

    construtores

---
+++ src/desafio3/Hokage.java
@@ -0,0 +1,14 @@
+package desafio3;
+
+public class Hokage {
+
+    String nome;
+    int idade;
+    boolean vivo;
+    //criando construtor
+    public Hokage(String nome, int idade){
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+}
--- src/desafio3/Main.java
+++ src/desafio3/Main.java
@@ -22,5 +22,10 @@ public class Main {
         boruto.AtivarJougan();
         boruto.AtivarOKarma();
 
+
+        Hokage Hashirama = new Hokage("Hashirama", 98);
+        System.out.println(Hashirama.nome);
+
+
     }
 }
